0631a458535c325bdf552c8c5684f3ce2ab5a2a0
Keb <vega25@live.fr>
2014-10-22 21:43:37 +0200

    Récupération de la room

--- parser.js
+++ parser.js
@@ -1,8 +1,25 @@
 exports.Parser = {
-    parser: function(conn, data){
-        switch(data[1]) {
-            //Traitement des données
+    room: '',
+    parser: function(c, data) {
+        if (data.indexOf('\n') > -1) {
+            var spl = data.split('\n');
+            for (var i = 0; i < spl.length; i++) {
+                if (spl[i].split('|')[1] == 'init') {
+                    //Pas de room
+                    this.room = '';
+                    break;
+                }
+                this.parser(c, spl[i]);
+            }
         }
+        var spl = data.split('|');
+        //Si spl[1] n'existe pas, on parse >
+        if (!spl[1]) {
+            spl = data.split('>');
+        }
+        if (!spl[1]) return;
+        //this.room, c'est là où "l'action se passe" :)
+        this.room = spl[1];
+        console.log(spl);
     }
 };
-
