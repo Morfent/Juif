519e8328fe1790685944f5416050043b59e8eda4
Keb <vega25@live.fr>
2014-10-24 00:22:21 +0200

    Récupération des messages d'une room
    Et affichage dans la console

--- parser.js
+++ parser.js
@@ -1,25 +1,40 @@
+/*
+ * Parser.js
+ * Ce fichier sert à traiter les données reçues
+ */
 exports.Parser = {
-    room: '',
+    room: '', // La room où se passe l'action, acualisée en permanence
     parser: function(c, data) {
+        if (!data) return;
         if (data.indexOf('\n') > -1) {
             var spl = data.split('\n');
             for (var i = 0; i < spl.length; i++) {
-                if (spl[i].split('|')[1] == 'init') {
-                    //Pas de room
-                    this.room = '';
-                    break;
-                }
+                /*if (spl[i].split('|')[1] && (spl[i].split('|')[1] === 'init' || spl[i].split('|')[1] === 'tournament')) {
+					this.room = '';
+					break;
+				}*/
                 this.parser(c, spl[i]);
             }
+            return;
         }
         var spl = data.split('|');
-        //Si spl[1] n'existe pas, on parse >
         if (!spl[1]) {
             spl = data.split('>');
+            if (!spl[1])
+                return;
+            this.room = spl[1];
         }
-        if (!spl[1]) return;
-        //this.room, c'est là où "l'action se passe" :)
-        this.room = spl[1];
-        console.log(spl);
+        switch (spl[1]) {
+            case 'c:':
+                console.log('(Room ' + this.room + ') ' + spl[3] + ': ' + spl[4]);
+                break;
+            case 'pm':
+                console.log('pm reçu !');
+                this.talk();
+                break;
+        }
+    },
+    talk: function(c, from, room, msg) {
+       //Censé envoyer un message
     }
 };
