e5e7d9794c5532bcd1262cc2b9ce815a52f64a2c
Keb <vega25@live.fr>
2014-10-28 22:27:15 +0100

    Écriture du fichier banlist.txt
    Les commandes !ab et !aub  sont maintenant finalisées. L'écriture du ficher banlist.txt est elle aussi OK.
    Le bannissement automatique à proprement parler n'est pas encore terminé.

--- commands.js
+++ commands.js
@@ -42,14 +42,41 @@ exports.Cmd = {
         this.talk(c, room, txt);
     },
     ab: function(c, params, from, room) {
-        if (!this.isRanked(from, '@')) return false;
+        //if (!this.isRanked(from, '@')) return false;
         if (!params) return false;
         var opts = params.split(',');
         if (opts[1].indexOf(',') > 1) return false;
-        var data = makeId(opts[0])+','+room+','+opts[1];
-        var banlist = fs.appendFile('data/banlist.csv', '\n'+data, function (err){
+        var data = makeId(opts[0]) + '|' + room + '|' + opts[1];
+        fs.appendFile('data/banlist.txt', '\n' + data, function(err) {
             console.log(err);
         });
-        this.talk(c, room, 'Ban permanant pour '+opts[0]+': '+opts[1]);
+        //On vire les sauts de lignes inutiles
+        var e = fs.readFileSync('data/banlist.txt').toString();
+        var output = e.replace(/^\s*$[\n\r]{1,}/gm, '');
+        fs.writeFileSync('data/banlist.txt', output);
+        this.talk(c, room, 'Ban permanant pour ' + opts[0] + ': ' + opts[1]);
+    },
+    aub: function(c, params, from, room) {
+        //if (!this.isRanked(from, '#')) return false;
+        if (!params) return false;
+        var success = false;
+        var banlist = fs.readFileSync('data/banlist.txt').toString().split('\n');
+        var editedbanlist = fs.readFileSync('data/banlist.txt').toString();
+        for (var i = 0; i < banlist.length; i++) {
+            var spl = banlist[i].toString().split('|');
+            if (makeId(params) == spl[0]) {
+                var search = spl[0] + '|' + spl[1] + '|' + spl[2];
+                var idx = editedbanlist.indexOf(search);
+                if (idx >= 0) {
+                    var output = editedbanlist.substr(0, idx) + editedbanlist.substr(idx + search.length);
+                    //On tej la ligne vide inutile qui fait planter le script
+                    var output = output.replace(/^\s*$[\n\r]{1,}/gm, '');
+                    fs.writeFileSync('data/banlist.txt', output);
+                    this.talk(c, room, spl[0] + ' a bien été débanni.');
+                    success = true;
+                }
+            }
+        }
+        if (success == false) this.talk(c, room, params + ' n\'est pas banni.');
     }
 };
