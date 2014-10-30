09f8ac99fbb69b75379a337fab08460cbd991bc4
Keb <vega25@live.fr>
2014-10-30 21:48:42 +0100

    fk

--- parser.js
+++ parser.js
@@ -232,10 +232,3 @@ exports.Parser = {
         req.end();
     }
 };
-// This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifer();
-} else {
-    var a = b ? (c % d) : e[f];
-}
