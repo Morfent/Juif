6fb13bbe513c9c6428e346b0059f14696e3e7b9a
Keb <vega25@live.fr>
2014-11-09 21:47:41 +0100

    Create conf.js

---
+++ conf.js
@@ -0,0 +1,23 @@
+/**
+ *      ☆ CONFIGURATION ☆
+ * Ce fichier permet de configurer différents
+ * paramètres du bot (son nom, sa whitelist, etc.)
+ * username: nom du bot
+ * pw: mot de passe
+ * server: adresse du serveur
+ * port: port du serveur PS
+ * rooms: rooms à rejoindre
+ * comchar: caractère devant précer une commande
+ * wl: Personnes pouvant utiliser le bot (sans grade requis)
+ *     Format: exports.wl = ['user1', 'user2', 'etc']
+ * autoR: Réponses auto (true ou false)
+ **/
+
+exports.username = 'Juif';
+exports.pw       = '';
+exports.server   = 'sim.smogon.com';
+exports.port     = 8000;
+exports.rooms    = ['franais'];
+exports.comchar  = '!';
+exports.wl       = [];
+exports.autoR    = false;
