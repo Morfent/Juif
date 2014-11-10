4300c1a0128b011c7467e723b95e1f8734e7b392
Keb <vega25@live.fr>
2014-11-10 23:26:44 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -237,7 +237,6 @@ exports.Cmd = {
         if (!this.isRanked(from, '@')) return false;
         var spl = params.split('::');
         if (!spl[0] || !spl[1] || !spl[2]) return false;
-        if (!isNaN(spl[2])) return false;
         var time = spl[2].replace(/\s/g, '')*60*1000;
         var id = spl[0].replace(/\s/g, '');
         var self = this;
