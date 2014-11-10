acadfe5a6db4fa023dd379d13c7fdcf1d20ff6a3
Keb <vega25@live.fr>
2014-11-10 14:08:42 +0100

    typo

--- conf.js
+++ conf.js
@@ -7,7 +7,7 @@
  * server: adresse du serveur
  * port: port du serveur PS
  * rooms: rooms à rejoindre
- * comchar: caractère devant précer une commande
+ * comchar: caractère devant précéder une commande
  * wl: Personnes pouvant utiliser le bot (sans grade requis)
  *     Format: exports.wl = ['user1', 'user2', 'etc']
  * autoR: Réponses auto (true ou false)
